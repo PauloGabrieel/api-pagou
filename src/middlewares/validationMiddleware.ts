48821046fcdb355ae9c413679f71d0d4b0f5f613
PauloGabrieel <gabrieeel.c@gmail.com>
2023-01-28 10:54:33 -0300

    refactor middleware

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,16 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import httpStatus from "http-status";
-import { createUserSchema } from "../schema/autheticationSchema";
+import { func, ObjectSchema } from "joi";
 import { invalidDataError } from "../errors/invalidDataError";
 
-export function validate(req: Request, res: Response, next: NextFunction){
-    const userBodyParam = req.body
-    const { error } = createUserSchema.validate(userBodyParam,{abortEarly: false});
+export function validateBody<T>(schema: ObjectSchema<T>): ValidationMiddleware {
+    return validate(schema, "body");
+};
 
-    if(!error) {
-        next()
-    }else {
-        const details = error.details.map(e => e.message)
-        res.status(httpStatus.BAD_REQUEST).send(invalidDataError(details));
-    }
-}
+export function validadeParams<T>(schame: ObjectSchema<T>): ValidationMiddleware {
+    return validate(schame, "params");
+};
+
+export function validateQuery<T>(schame: ObjectSchema<T>):ValidationMiddleware {
+    return validate(schame, "query");
+};
+
+function validate(schema: ObjectSchema, type: "body" | "params" | "query") {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const { error } = schema.validate(req[type], { abortEarly: false });
+    
+        if(!error) {
+            next()
+        }else {
+            const details = error.details.map(e => e.message)
+            res.status(httpStatus.BAD_REQUEST).send(invalidDataError(details));
+        };
+    };
+
+};
+
+type ValidationMiddleware = (req: Request, res: Response, next: NextFunction)=> void;
