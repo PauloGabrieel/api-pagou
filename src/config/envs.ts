dea854099d1957c8ed0026bfe47a893315b0b571
PauloGabrieel <gabrieeel.c@gmail.com>
2023-02-23 14:44:34 -0300

    Add scripts and configs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv'
 
 export default function loadEnvs () {
-  let path = '.env'
+  let path = '.env.development'
 
   if (process.env.NODE_ENV === 'test') {
     path = '.env.test'
+  }
+  if (process.env.NODE_ENV === 'production') {
+    path = '.env.production'
   }
 
   dotenv.config({ path })
